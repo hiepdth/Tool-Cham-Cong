b708e7b0bc07518b63ce168e4375fed78da4408b
hiepdth <dinhhiepxtb@gmail.com>
2022-01-09 12:07:25 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+//        File file = new File("D:\\0.Dinh Hiep\\Du lieu cham cong\\Dulieuchamcong\\7-9-2020\\Thang_8.dat");
+        File file = new File("D:\\0.Dinh Hiep\\Du lieu cham cong\\Dulieuchamcong\\01_12_2021den31_12_2021\\01_12_2021den31_12_2021.dat");
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String st;
+//            String s = br.readLine();
+//            splitUsingTokenizer(s);
+            int count = 0;
+            while ((st = br.readLine()) != null) {
+                if (st.contains("1005")) {
+                    String s[] = splitUsingTokenizer(st);
+
+                    System.out.print(s[1] + " " +s[2]+ "\t" + s[4]+ "\t");
+                    if (s[4].equals("1")){
+                        System.out.println();
+                    }
+//                    count++;
+//                    if (count == 2){
+//                        System.out.println();
+//                        count = 0;
+//                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] splitUsingTokenizer(String s) {
+        String arr[] = new String[10];
+        int i = 0;
+        StringTokenizer st =
+                new StringTokenizer(s);
+        while (st.hasMoreTokens()) {
+            arr[i] = st.nextToken();
+            i++;
+        }
+        return arr;
+    }
+}
